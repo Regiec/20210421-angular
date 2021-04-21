a940938e223d30aed08ecf19c96e3d08adf51d7b
Jabi <michal.jabi@gmail.com>
2021-04-21 15:11:27 +0200

    feat: my first header component

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  template: `
+    <header class="jumbotron" (click)="addExclamationToTile()">
+      <h1 [class]="h1Class"> {{ title }}  <span *ngIf="h1Class === 'red'" > ? </span> </h1>
+      <h2 *ngFor="let x of [1,2,3,4]"> Podtyuł {{x}} </h2>
+    </header>
+  `,
+  styles: [
+    `
+      .red {
+        color: red
+      }
+      .green {
+        color: green
+      }
+    `
+  ]
+})
+export class HeaderComponent implements OnInit {
+
+  h1Class = 'red';
+  title = 'Auction Portal';
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // this.title = '!!';
+  }
+
+  addExclamationToTile(): void {
+    this.title += '!';
+    this.h1Class = (this.h1Class === 'red') ? 'green' : 'red';
+    // ? zmieniaj nazwę klasy h1Class z red na green i odwrotnie tutaj!
+  }
+
+}
